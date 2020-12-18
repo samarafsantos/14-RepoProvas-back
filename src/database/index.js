0be8c8c2329de3ddde7525d69f8bb945bb539aea
samarafsantos <samarasantos@poli.ufrj.br>
2020-12-18 18:54:02 -0300

    merge

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const connection = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-});
+const dbConfig = { connectionString: process.env.DATABASE_URL };
+
+const connection = new Pool(dbConfig);
 
 module.exports = connection;
